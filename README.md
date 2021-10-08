ea0b579a6070126a975683f228a2d033cd0ffa30
Bil Moorhead <bil@alitheon.com>
2021-10-08 10:19:14 -0700

    awkward first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ServiceCenter
+
+A description of this package.
---
+++ Tests/ServiceCenterTests/ServiceCenterTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ServiceCenter
+
+final class ServiceCenterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ServiceCenter().text, "Hello, World!")
+    }
+}
