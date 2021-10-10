a53b9116a6f9730ab31b25efd86f4c6f329e0596
Bil Moorhead <bil@alitheon.com>
2021-10-10 01:54:45 -0700

    made the JSON facade public

--- Sources/ServiceCenter/JSON.swift
+++ Sources/ServiceCenter/JSON.swift
@@ -8,22 +8,19 @@
 import Foundation
 import DateFormats
 
-final class JSON {
+public final class JSON {
 	
-	static var encoder: JSONEncoder = {
+	public static var encoder: JSONEncoder = {
 		
 		let encoder = JSONEncoder()
 		encoder.outputFormatting = .prettyPrinted
 		encoder.dateEncodingStrategy = .formatted(.iso8601Full)
-//		encoder.dataEncodingStrategy = .custom {
-//			var container = $1.singleValueContainer()
-//			try container.encode($0.hex)
-//		}
 
 		return encoder
 		
 	}()
-	static var encoderNWK: JSONEncoder = {
+	
+	public static var encoderNWK: JSONEncoder = {
 		
 		let encoder = JSONEncoder()
 		encoder.dateEncodingStrategy = .formatted(.iso8601Full)
@@ -32,30 +29,53 @@ final class JSON {
 		
 	}()
 	
+	public static var encoderHEX: JSONEncoder = {
+		
+		let encoder = JSONEncoder()
+		encoder.outputFormatting = .prettyPrinted
+		encoder.dateEncodingStrategy = .formatted(.iso8601Full)
+		encoder.dataEncodingStrategy = .custom {
+			var container = $1.singleValueContainer()
+			try container.encode($0.hex)
+		}
+
+		return encoder
+		
+	}()
+	
 	
 	//
 	//
 	//
 	
 	
-	static var decoder: JSONDecoder = {
+	public static var decoder: JSONDecoder = {
 		
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .formatted(.iso8601Full)
-//		decoder.dataDecodingStrategy = .custom {
-//			let containter = try $0.singleValueContainer()
-//			let string = try containter.decode(String.self)
-//			return Data(hexString: string)
-//		}
 
 		return decoder
 		
 	}()
 
-	static var decoderNWK: JSONDecoder = {
+	public static var decoderNWK: JSONDecoder = {
 		
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .formatted(.iso8601Full)
+
+		return decoder
+		
+	}()
+
+	public static var decoderHEX: JSONDecoder = {
+		
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .formatted(.iso8601Full)
+		decoder.dataDecodingStrategy = .custom {
+			let containter = try $0.singleValueContainer()
+			let string = try containter.decode(String.self)
+			return Data(hexString: string)
+		}
 
 		return decoder
 		
